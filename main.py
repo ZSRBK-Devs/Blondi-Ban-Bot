69342b40ba5fc92506883c34f612259d8b1c2086
Olaf Łuczak <olafhd4@gmail.com>
2021-03-09 07:27:13 +0000

    New features

--- main.py
+++ main.py
@@ -9,10 +9,19 @@ client = commands.Bot(command_prefix='$')
 async def on_ready():
     print('Można banować')
 
-@client.command()
-async def Voteban(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
+@client.command(pass_ctx=True)
+@commands.has_permissions(ban_members=True)
+async def Voteban(ctx, member: discord.Member):
+    await member.ban()
     await ctx.send('Blondi Zbanowany XDDD!!')
+
+@client.command()
+async def rajdówki(ctx):
+  await ctx.send('Rajdówki nie driftują')
+
+@client.command()
+async def golf(ctx):
+  await ctx.send('Ej chłopaki golfa kupiłem')
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
