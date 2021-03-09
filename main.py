68c579ca747564b8ec4b7c55d2a0f3cdda1a5e37
Olaf Łuczak <olafhd4@gmail.com>
2021-03-09 06:56:21 +0000

    Improved code appearance.

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ from keep_alive import keep_alive
 
 client = commands.Bot(command_prefix='$')
 
-
 @client.event
 async def on_ready():
     print('Można banować')
-
 
 @client.command()
 async def Voteban(ctx, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
     await ctx.send('Blondi Zbanowany XDDD!!')
-
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
